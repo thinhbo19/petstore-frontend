674fd8856688d0b6545f0cf284d7a745b6aadefa
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-09-05 21:48:58 +0700

    layout

--- src/app/(user)/layout.js
+++ src/app/(user)/layout.js
@@ -1,15 +1,11 @@
-import ChatBox from "@/src/component/ChatBox/ChatBox";
 import Footer from "@/src/component/Footer/Footer";
-import Navbar from "@/src/component/Navbar/Navbar";
 import ScrollButton from "@/src/component/ScrollButton/ScrollButton";
 
 export default function UserLayout({ children }) {
   return (
     <>
-      <Navbar />
       {children}
       <Footer />
-      {/* <ChatBox /> */}
       <ScrollButton />
     </>
   );
